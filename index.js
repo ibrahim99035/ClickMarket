ca32c7eeb336f3500258ca9d93dbeac89c43ea29
ibrahim <ebrahimaboeita990@gmail.com>
2024-02-29 17:51:23 +0200

    Regtration process initiation

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express =  require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-const usersRoute = require('./routes/users');
+const usersRoute = require('./routes/user');
+const authRoute = require('./routes/auth');
+
 
 const app = express();
 
@@ -14,7 +16,8 @@ mongoose.connect(
 
 
 app.use(express.json());
-app.use('/api/users', usersRoute);
+app.use('/api/users', usersRoute); 
+app.use('/api/auth', authRoute);
 
 app.listen(process.env.PORT || 5000, ()=>{
     console.log(`The Server is running on port ${process.env.PORT}`);
