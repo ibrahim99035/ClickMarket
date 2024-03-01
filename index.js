d940ba17bb58bbbf21fcdf7bd95689f564a753d7
ibrahim <ebrahimaboeita990@gmail.com>
2024-03-01 14:53:15 +0200

    Database connection Isolation

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ const dotenv = require('dotenv');
 const usersRoute = require('./routes/user'); 
 const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
+const cartRoute = require('./routes/cart');
+const orderRoute = require('./routes/order');
 
 const app = express();
 
 dotenv.config();
-const MongoConnection = require('./config/database')
+require('./config/database')
 
 app.use(express.json());
+
 app.use('/api/users', usersRoute); 
 app.use('/api/auth', authRoute);
 app.use('/api/products', productRoute);
+app.use('/api/cart', cartRoute);
+app.use('/api/orders', orderRoute);
 
 app.listen(process.env.PORT || 5000, ()=>{
     console.log(`The Server is running on port ${process.env.PORT}`);
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const {verifyToken} = require('../middleware/TokenVerification');
-const {verifyAuth} = require('../middleware/Authrization')
-const {verifyAdmin} = require('../middleware/AdminVerification')
+const {verifyAuth} = require('../middleware/Authrization');
+const {verifyAdmin} = require('../middleware/AdminVerification');
 
 // Update User
 router.put('/:id', verifyToken, verifyAuth, async(req, res) => {
